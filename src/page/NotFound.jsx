c4209be5026a107cc856e5ac31eab92b434a57e9
Iasmin Dias <iasmin.dias87@gmail.com>
2022-09-28 11:48:43 -0300

    :question: Beginning of NotFound Page development

--- src/page/NotFound.jsx
+++ src/page/NotFound.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
+import  Logo  from './../components/Logo'
+
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className='position-absolute flex-column w-100 h-100 d-flex justify-content-center align-items-center gap-5'>
+      <Logo size={70} color={'light'}/>
+      <div className='row w-100 h-70 d-flex justify-content-center align-items-center'>
+        <span className='col d-flex justify-content-end' style={{fontSize: '15rem'}}>4</span>
+        <img className='col' src="src\assets\images\broken-vinyl-record.png" alt="" style={{width: 'auto'}}/>
+        <span className='col' style={{fontSize: '15rem'}}>4</span>
+      </div>
+    </div>
   )
 }
 
