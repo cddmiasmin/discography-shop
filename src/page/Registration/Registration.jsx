4e4921bf9a5437d0d3c6434fc0e2d075de0f9a25
Iasmin Dias <iasmin.dias87@gmail.com>
2022-09-28 09:26:18 -0300

    :woman_mechanic: Developed Registration Page

--- src/page/Registration/Registration.jsx
+++ src/page/Registration/Registration.jsx
@@ -21,9 +21,7 @@ const Registration = () => {
   const {
     data,
     imageNumber,
-    SetImageNumber,
     bannerInPortraitOrLandscapeMode,
-    SetBannerInPortraitOrLandscapeMode,
     WhatOrientationIsTheScreenInNow,
     ChooseImageForTheBanner,
   } = useLoginRegistration();
@@ -38,7 +36,7 @@ const Registration = () => {
   useEffect(() => {
     document.body.style.setProperty('--colorIsWhiteOrBlack', `${colorIsWhiteOrBlack}`);
     document.body.style.setProperty('--color', `${color}`);
-  }, [color, colorRGBA])
+  }, [color])
 
 
   return (
@@ -58,7 +56,7 @@ const Registration = () => {
       <div
         id='card-registration'
         className='d-flex justify-content-center align-items-center'
-        style={{width: '35%', height: '80%' }}
+        style={{ width: '35%', height: '80%' }}
       >
         <div className='h-100 w-100 d-flex flex-column flex-wrap justify-content-center align-items-center gap-3'>
           <Logo size={70} color={colorIsDarkOrLight} />
@@ -72,44 +70,66 @@ const Registration = () => {
                 <p className='m-0' style={{ fontSize: 'medium' }}>Já tem uma conta?</p>
                 <Link to='/login'
                   className={`d-flex justify-content-center text-decoration-none text-${colorIsDarkOrLight}`}
-                  style={{fontSize: 'medium' }}
+                  style={{ fontSize: 'medium' }}
                 ><u>ENTRE</u></Link>
               </div>
-
             </div>
 
             <form
-              id='form-login'
+              id='form-registration'
               action=""
               className='d-flex w-100 flex-column justify-content-center align-items-center gap-2'
             >
+              <div id='customer-name-field' 
+                className='element-width overflow-hidden d-flex flex-row justify-content-center align-items-center gap-1'
+                style={{height: '5.5vh'}}
+              >
+                <input
+                  className='rounded'
+                  type="text" id='field-name' required pattern='[a-Z]{3,}' placeholder='NOME'
+                  style={{ width: '39%' }}
+                />
+                <input
+                  className='rounded'
+                  type="text" id='field-last-name' required pattern='[a-Z]{3,}' placeholder='SOBRENOME'
+                  style={{ width: '59%' }}
+                />
+              </div>
               <input
-                className='rounded'
+                className='element-width rounded'
                 type="email" name="fieldEmail" id="field-email" placeholder='E-mail' required="required"
                 pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
               />
               <input
-                className='rounded'
-                type="password" name="fieldPassword" id="field-password" placeholder='Senha' required="required"
+                className='element-width rounded'
+                type="text" name="fieldPassword" id="field-password" placeholder='Senha' required="required"
                 pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,}"
                 title='Sua senha deve conter 4 ou mais caracteres com pelo menos um número, uma letra maiúscula e uma minúscula'
               />
-              <div
-                className='d-flex justify-content-end m-0'
-                style={{ width: '55%', fontSize: 'small' }}
+              <input
+                className='element-width rounded'
+                type="text" name="fieldPassword" id="field-password" placeholder='CONFIRME SUA SENHA' required="required"
+                pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,}"
+                title='Sua senha deve conter 4 ou mais caracteres com pelo menos um número, uma letra maiúscula e uma minúscula'
+              />
+              <div id='terms-of-use'
+                className='element-width d-flex flex-row justify-content-star align-items-center gap-1 m-1'
+                style={{cursor: 'pointer', fontSize: 'small'}}
               >
-                <button
-                  className={`text-${colorIsDarkOrLight} bg-transparent`}
-                  type='button'
-                > ESQUECI MINHA SENHA</button>
+                <input type="checkbox" name="cb-terms-of-use" id="cb-terms-of-use" style={{width: 'auto', height: 'auto'}}/>
+                <span className='m-0 p-0'>Eu li e concordo com os</span>
+                <a
+                  href="https://pbs.twimg.com/media/FCWcK8FWUAY4PaP?format=jpg&name=small"
+                  style={{ color: `${color}` }}
+                >Termos de Serviço</a>
               </div>
-              <div className={`bg-${colorIsDarkOrLight} rounded `} style={{ height: '0.3vh', width: '55%' }} />
+              <div className={`bg-${colorIsDarkOrLight} rounded element-width`} style={{ height: '0.3vh' }} />
               <button
-                className='d-flex justify-content-center align-items-center rounded m-1'
-                style={{ width: '55%', height: '5vh', fontSize: 'larger' }}
+                className='element-width d-flex justify-content-center align-items-center rounded m-1'
+                style={{ height: '5vh', fontSize: 'larger' }}
                 type="submit"
               >
-                ENTRAR
+                CADASTRAR-SE
               </button>
             </form>
           </div>
