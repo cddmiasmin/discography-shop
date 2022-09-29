e28912cb72bb953b0acf3256f0b0df9d5e81d77f
Iasmin Dias <iasmin.dias87@gmail.com>
2022-09-29 07:29:08 -0300

    :abcd: Displaying artists in alphabetical order

--- src/page/Artists.jsx
+++ src/page/Artists.jsx
@@ -21,6 +21,8 @@ const Artists = () => {
 
   const artistNames = SortingArtistNamesInAlphabeticalOrder(dataArtist);
 
+  console.log(artistNames[`${'A'}`] === undefined)
+
   return (
     <div
       className='d-flex flex-column w-100 justify-content-center align-items-center bg-black'
@@ -37,7 +39,8 @@ const Artists = () => {
           className='d-flex flex-wrap justify-content-center align-items-center gap-2'
           style={{ width: '85%', borderBottom: '1px solid white', padding: '3vh' }}
         >
-          {alphabetAndNumbers.map((character, key) => (
+        {alphabetAndNumbers.map((character, key) => (
+          (artistNames[`${character}`] !== undefined &&
             <a href={'#' + character}
               key={key}
               className='d-flex flex-wrap rounded justify-content-center align-items-center text-white bg-transparent text-decoration-none'
@@ -45,47 +48,38 @@ const Artists = () => {
             >
               <h5 className='m-0'>{character}</h5>
             </a>
-          ))}
+          )))}
         </section>
 
         {alphabetAndNumbers.map((character, key) => (
-          <section
-            key={key}
-            id={character}
-            className='d-flex w-100 flex-column flex-wrap justify-content-star align-items-center gap-2'
-            style={{ marginTop: '5vh', paddingBottom: '4vh' }}
-          >
-
-            <div
-              className='d-flex flex-column flex-wrap justify-content-center align-items-center gap-2'
-              style={{ width: '85%', borderBottom: '1px solid white' }}
+          (artistNames[`${character}`] !== undefined &&
+            <div section
+              key={key}
+              id={character}
+              className='d-flex w-100 flex-column flex-wrap justify-content-star align-items-center gap-2'
+              style={{ marginTop: '5vh', paddingBottom: '4vh' }}
             >
-              <h2 className='d-flex w-100 justify-content-star align-items-center ms-3'>{character}</h2>
-            </div><div
-              className='d-flex flex-row flex-wrap justify-content-star align-items-center gap-2'
-              style={{ width: '85%' }}
-            >
-              <div className='d-flex flex-column justify-content-center align-items-center gap-2 p-2'>
-                <img style={{ height: '20vh', width: '20vh', borderRadius: '6vh' }} src="https://i.pinimg.com/originals/c5/5f/b1/c55fb130aafc28a51618390e6325bd47.jpg" alt="" />
-                <h6 className='m-0'>FLORENCE AND THE MACHINE</h6>
+              <div
+                className='d-flex flex-column flex-wrap justify-content-center align-items-center gap-2'
+                style={{ width: '85%', borderBottom: '1px solid white' }}
+              >
+                <h2 className='d-flex w-100 justify-content-star align-items-center ms-3'>{character}</h2>
               </div>
 
-              <div className='d-flex flex-column justify-content-center align-items-center gap-2 p-2'>
-                <img style={{ height: '20vh', width: '20vh', borderRadius: '6vh' }} src="https://i.pinimg.com/originals/c5/5f/b1/c55fb130aafc28a51618390e6325bd47.jpg" alt="" />
-                <h6 className='m-0'>FLORENCE AND THE MACHINE</h6>
-              </div>
-
-              <div className='d-flex flex-column justify-content-center align-items-center gap-2 p-2'>
-                <img style={{ height: '20vh', width: '20vh', borderRadius: '6vh' }} src="https://i.pinimg.com/originals/c5/5f/b1/c55fb130aafc28a51618390e6325bd47.jpg" alt="" />
-                <h6 className='m-0'>FLORENCE AND THE MACHINE</h6>
-              </div>
-
-              <div className='d-flex flex-column justify-content-center align-items-center gap-2 p-2'>
-                <img style={{ height: '20vh', width: '20vh', borderRadius: '6vh' }} src="https://i.pinimg.com/originals/c5/5f/b1/c55fb130aafc28a51618390e6325bd47.jpg" alt="" />
-                <h6 className='m-0'>FLORENCE AND THE MACHINE</h6>
+              <div
+                className='d-flex flex-row flex-wrap justify-content-star align-items-center gap-2'
+                style={{ width: '85%' }}
+              >
+                {artistNames[`${character}`].map((artist, key) => (
+                <div className='d-flex flex-column justify-content-center align-items-center gap-2 p-2'>
+                  <img style={{ height: '20vh', width: '20vh', borderRadius: '6vh' }}
+                    src={artist.icon} alt={`${artist.name} icon`} />
+                  <h6 className='m-0'>{artist.name}</h6>
+                </div>
+                ))}
               </div>
             </div>
-          </section>
+          )
         ))}
 
 
@@ -99,7 +93,7 @@ const Artists = () => {
         <br />
         <Footer colorIsDarkOrLight={'dark'} color={'white'} colorIsWhiteOrBlack={'black'} />
       </div>
-    </div>
+    </div >
   )
 }
 
