c93b3125ac8d6727c0b652cf180f11f0ab9e81f7
Iasmin Dias <iasmin.dias87@gmail.com>
2022-10-01 03:12:33 -0300

    :arrows_counterclockwise:	Data update

---
+++ src/data/dataBackgrounds.jsx
@@ -0,0 +1,28 @@
+export const dataBackgrounds = [
+    {
+        portrait: [
+            {imgUrl: 'src/assets/images/Backgrounds/portrait/portrait-banner-1.jpg'},
+            {imgUrl: 'src/assets/images/Backgrounds/portrait/portrait-banner-2.jpg'},
+            {imgUrl: 'src/assets/images/Backgrounds/portrait/portrait-banner-3.jpg'},
+            {imgUrl: 'src/assets/images/Backgrounds/portrait/portrait-banner-4.jpg'},
+            {imgUrl: 'src/assets/images/Backgrounds/portrait/portrait-banner-5.jpg'},
+            {imgUrl: 'src/assets/images/Backgrounds/portrait/portrait-banner-6.jpg'},
+            {imgUrl: 'src/assets/images/Backgrounds/portrait/portrait-banner-7.jpg'},
+            {imgUrl: 'src/assets/images/Backgrounds/portrait/portrait-banner-8.jpg'},
+            {imgUrl: 'src/assets/images/Backgrounds/portrait/portrait-banner-9.jpg'},
+        ],
+        landscape: [
+            {imgUrl: 'src/assets/images/Backgrounds/landscape/landscape-banner-1.jpg'},
+            {imgUrl: 'src/assets/images/Backgrounds/landscape/landscape-banner-2.jpg'},
+            {imgUrl: 'src/assets/images/Backgrounds/landscape/landscape-banner-3.jpg'},
+            {imgUrl: 'src/assets/images/Backgrounds/landscape/landscape-banner-4.jpg'},
+            {imgUrl: 'src/assets/images/Backgrounds/landscape/landscape-banner-5.jpg'},
+            {imgUrl: 'src/assets/images/Backgrounds/landscape/landscape-banner-6.jpg'},
+            {imgUrl: 'src/assets/images/Backgrounds/landscape/landscape-banner-7.jpg'},
+            {imgUrl: 'src/assets/images/Backgrounds/landscape/landscape-banner-8.jpg'},
+            {imgUrl: 'src/assets/images/Backgrounds/landscape/landscape-banner-9.jpg'},
+            {imgUrl: 'src/assets/images/Backgrounds/landscape/landscape-banner-10.jpg'},
+            {imgUrl: 'src/assets/images/Backgrounds/landscape/landscape-banner-11.jpg'},
+        ],
+    }
+]
