a75d1232d7b8e28478baaf796bbdcfbef6f636b1
Iasmin Dias <iasmin.dias87@gmail.com>
2022-10-06 22:06:49 -0300

    :woman_technologist: test

--- src/components/Header/Desktop.jsx
+++ src/components/Header/Desktop.jsx
@@ -1,22 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { Link } from 'react-router-dom'
 
 import Logo from '../Logo'
 import Cart from '../Cart'
 
+import { Popover } from '@mui/material';
 import { Tooltip } from '@mui/material';
-import { Drawer, Box, Typography } from '@mui/material';
 
-import { BiMenu } from 'react-icons/bi';
-import { FaTrash } from 'react-icons/fa';
 import { HiSearch } from 'react-icons/hi';
 import { IoPerson } from 'react-icons/io5';
+import { RiSearch2Fill } from 'react-icons/ri'
 import { BsFillBagFill } from 'react-icons/bs';
-import { RiCloseCircleFill } from 'react-icons/ri';
-import { MdKeyboardArrowDown } from 'react-icons/md';
+
 
 const Desktop = (props) => {
+
+    const [anchorDesk, SetAnchorDesk] = useState(null);
+
+    
+    const openSeachButtonIcon = document.getElementById('open-search');
+    const seachButtonIcon = document.getElementById('search');
+
+    const handleClick = (event) => {
+        openSeachButtonIcon.style.display = 'none'
+        seachButtonIcon.style.display = 'flex'
+
+        SetAnchorDesk((event.currentTarget))
+    };
+
+    const handleClose = () => {
+        openSeachButtonIcon.style.display = 'flex'
+        seachButtonIcon.style.display = 'none'
+        SetAnchorDesk(null)
+    }
+
     return (
         <div
             className={`flex-row container-fluid d-flex position-absolute top-0 start-0
@@ -36,11 +54,38 @@ const Desktop = (props) => {
 
             <div id='IconOption' className='col d-flex justify-content-end align-items-center me-4 gap-4'>
                 <Tooltip title="Buscar">
-                    <button className={`bg-transparent text-${props.colorIsDarkOrLight}`}><HiSearch className='fs-5 ' /></button>
+                    <div>
+                        <button type='button' id='open-search' 
+                            onClick={handleClick} style={{display: 'flex'}}
+                            className={`bg-transparent text-${props.colorIsDarkOrLight}`}><HiSearch className='fs-5 ' /></button>
+                        <button type='submit' id='search' 
+                            onClick={handleClick} style={{display: 'none'}}
+                            className={`bg-transparent text-${props.colorIsDarkOrLight}`}><RiSearch2Fill className='fs-5 ' /></button>
+                    </div>
                 </Tooltip>
+
                 <Tooltip title="Perfil">
                     <button className={`bg-transparent text-${props.colorIsDarkOrLight}`}><IoPerson className='fs-5 ' /></button>
                 </Tooltip>
+
+                <Popover
+                    id='popover-profile-desktop'
+                    open={Boolean(anchorDesk)}
+                    anchorEl={anchorDesk}
+                    onClose={handleClose}
+                    anchorOrigin={{
+                        vertical: 'center',
+                        horizontal: 'left',
+                    }}
+                    transformOrigin={{
+                        vertical: 'center',
+                        horizontal: 'right',
+                    }}
+                >
+                    <input type="text" name="search-desk" id="search-desk" pattern='[a-Z]{3,}' />
+                </Popover>
+
+
                 <Tooltip
                     title="Carrinho"
                     onClick={() => props.setIsDrawerCartOpen(true)}
