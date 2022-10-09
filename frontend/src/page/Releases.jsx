efb8138f2f815c8c6f5d71dc79c8f3db353340d4
Iasmin Dias <iasmin.dias87@gmail.com>
2022-10-09 07:06:51 -0300

    :woman_artist: Layout redesign of PreOrder and Releases Pages

--- frontend/src/page/Releases.jsx
+++ frontend/src/page/Releases.jsx
@@ -9,16 +9,7 @@ import { useChooseBackgroundImage } from './../functions/useChooseBackgroundImag
 
 import { dataArtist } from '../data/dataArtist'
 
-import { Swiper, SwiperSlide } from "swiper/react";
-
-import "swiper/css";
-import "swiper/css/grid";
-import "swiper/css/pagination";
-
-import { Grid, Pagination } from "swiper";
-
-
-const Realeses = () => {
+const Releases = () => {
 
   const {
     color,
@@ -42,55 +33,49 @@ const Realeses = () => {
 
   useEffect(() => getColor(bannerInPortraitOrLandscapeMode[imageNumber].imgUrl))
 
-  useEffect(() => {
-    document.body.style.setProperty('--colorIsWhiteOrBlack', `${colorIsWhiteOrBlack}`);
-    document.body.style.setProperty('--color', `${color}`);
-  }, [color])
-
   return (
-    <div className='position-absolute w-100 h-100 d-flex justify-content-center align-items-center flex-column'>
-      <Header colorIsDarkOrLight={'light'} color={color} colorIsWhiteOrBlack={colorIsWhiteOrBlack} />
+    <div className='w-100 h-100 d-flex justify-content-center align-items-center flex-column bg-black'>
 
-      <div id='bg-image-header' className='position-absolute w-100 h-100' style={{ top: 0, opacity: '35%', zIndex: -200 }}>
-        <img className='w-100 h-100' src={bannerInPortraitOrLandscapeMode[imageNumber].imgUrl} alt="" />
-      </div>
+      <div id='bg-releases'
+        style={{
+          backgroundImage: `url(${bannerInPortraitOrLandscapeMode[imageNumber].imgUrl})`,
+          width: '100%', height: '30vh', backgroundPosition: 'center', backgroundSize: 'cover',
+          opacity: '28%', borderRadius: '0px 0px 15px 15px', borderBottom: '3px solid var(--color)'
+        }} />
 
-      <div className='position-absolute flex-column w-100 d-flex justify-content-center align-items-center' style={{ zIndex: 1, top: '12vh', zIndex: 1 }}>
-      <h1>LANÇAMENTOS</h1>
-        <Swiper
-          slidesPerView= 'auto'
-          slides
-          grid={{
-            rows: 2,
-          }}
-          spaceBetween={30}
-          pagination={{
-            clickable: true,
-          }}
-          modules={[Grid, Pagination]}
-          className="mySwiper d-flex justify-content-center align-items-center"
-          style={{ width: '95%', height: '80vh' }}
-        >
-          {dataArtist[1].album.map((album, key) => (
-            <SwiperSlide
-              key={key}
-              className='d-flex justify-content-center align-items-center'
-              style={{width: '220px', height: '220px'}}
-            >
-              <Album
-                key={key}
-                cover={album.cover}
-                artist={dataArtist[1].name}
-                name={album.name}
-                year={album.year}
-              />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      <Footer colorIsDarkOrLight={colorIsDarkOrLight} color={color} colorIsWhiteOrBlack={colorIsWhiteOrBlack} /> 
+      <Header colorIsDarkOrLight={'light'} color={color} colorIsWhiteOrBlack={'white'} />
+      <div id='container-releases' className='d-flex w-100 flex-column justify-content-end align-items-center position-absolute'
+        style={{ color: 'white', top: '15vh', zIndex: '2' }}
+      >
+        <h1>LANÇAMENTOS</h1>
       </div>
+      <div className='d-flex flex-row gap-2 justify-content-center align-items-center p-4'>
+        <h5 className='m-0'>ORDENAR POR:</h5>
+        <select style={{ width: '25vh' }}>
+          <option value="DL">Data de Lançamento</option>
+          <option value="MnP">Menor Preço</option>
+          <option value="MmP">Maior Preço</option>
+          <option value="MV">Mais Vendidos</option>
+          <option value="MD">Melhor Desconto</option>
+          <option value="AZ">A - Z</option>
+          <option value="ZA">Z - A</option>
+        </select>
+      </div>
+      <div className={`rounded `} style={{ backgroundColor: 'var(--color)', height: '0.5vh', width: '85%' }} />
+      <div className='d-flex flex-wrap justify-content-center align-items-end gap-5' style={{ marginTop: '4vh', marginBottom: '4vh', width: '85%' }}>
+        {dataArtist[1].album.map((album, key) =>
+          <Album
+            key={key}
+            cover={album.cover}
+            artist={dataArtist[1].name}
+            name={album.name}
+            year={album.year}
+          />
+        )}
+      </div>
+      <Footer colorIsDarkOrLight={colorIsDarkOrLight} color={color} colorIsWhiteOrBlack={colorIsWhiteOrBlack} />
     </div>
   )
 }
 
-export default Realeses
+export default Releases
