ddd12ea7a7413429dcea46d81cab633779e4df2f
Iasmin Dias <iasmin.dias87@gmail.com>
2022-10-17 12:14:10 -0300

    :abacus: Server created and running

---
+++ backend/src/server.js
@@ -0,0 +1,19 @@
+require('dotenv').config({
+    path: 'variaveis.env'
+});
+
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const routes = require('./routes.js');
+
+const server = express();
+server.use(cors());
+server.use(bodyParser.urlencoded ({
+    extend: false
+}))
+
+server.listen(process.env.PORT, () => {
+    console.log(`The server is running on http://locallhost:${process.env.PORT}`);
+});
