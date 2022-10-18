ea8fc852ac1c7937933cdc8c6eeb015410bc241f
Iasmin Dias <iasmin.dias87@gmail.com>
2022-10-18 17:33:50 -0300

    :dna: Artist: Architecture Integration

--- backend/src/controllers/ArtistController.js
+++ backend/src/controllers/ArtistController.js
@@ -2,18 +2,23 @@ const ArtistService = require('./../services/ArtistService');
 
 module.exports = {
     ShowArtist: async (req, res) => {
-        let json = {error: '', result: []}
+        let json = {result:[]}
 
         let slug = req.params.slug;
         let artist = await ArtistService.ShowArtist(slug);
 
-        if(artist) json.result = artist;
+        if(artist) json.result = {
+            code: artist.cd_artist,
+            slug: artist.slug_artist,
+            name: artist.nm_artist,
+            icon: artist.img_icon
+        };
         
         res.json(json);
     },
 
     ListArtists: async (req, res) => {
-        let json = {error:'', result:[]};
+        let json = {result:[]};
 
         let artists = await ArtistService.ListArtists();
 
@@ -22,7 +27,7 @@ module.exports = {
                 code: artists[i].cd_artist,
                 slug: artists[i].slug_artist,
                 name: artists[i].nm_artist,
-                imgIcon: artists[i].img_icon
+                icon: artists[i].img_icon
             });
         }
 
