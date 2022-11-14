c5c8f4a8c14baf03f5590cd47a02f857d3d6f4a6
Iasmin Dias <iasmin.dias87@gmail.com>
2022-11-14 03:52:08 -0300

    :broom: Resetting the database

--- database/tables/album.sql
+++ database/tables/album.sql
@@ -1,7 +1,6 @@
 create table tb_cassandra_album(
     cd_album bigint primary key auto_increment,
     fk_artist bigint,
-    fk_category bigint,
     slug_album varchar(120) not null,
     nm_album varchar(60) not null,
     dt_album timestamp not null,
--- database/tables/artist.sql
+++ database/tables/artist.sql
@@ -1,6 +1,8 @@
 create table tb_cassandra_artist(
     cd_artist bigint primary key auto_increment,
     slug_artist varchar(100) not null,
+    fk_category bigint,
     nm_artist varchar(50) not null,
-    url_icon varchar(100) not null
+    url_icon varchar(100) not null,
+    foreign key (fk_category) references tb_cassandra_category(cd_category)
 );
