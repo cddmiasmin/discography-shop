55ffd900310123f7e465f1f604b339ad736adfd0
Iasmin Dias <iasmin.dias87@gmail.com>
2022-11-18 18:58:17 -0300

    :mag: Search component

--- frontend/src/components/Header/Desktop.jsx
+++ frontend/src/components/Header/Desktop.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useLayoutEffect } from 'react'
+import React, { useRef, useState } from 'react'
 
 import './header.css'
 
@@ -6,57 +6,14 @@ import { Link, useNavigate } from 'react-router-dom'
 
 import Logo from '../Logo'
 import Cart from '../Cart'
+import Search from '../Search/Search'
 
 import { Alert, Snackbar, Tooltip } from '@mui/material';
 
-import { HiSearch } from 'react-icons/hi';
 import { IoPerson } from 'react-icons/io5';
 import { BsFillBagFill } from 'react-icons/bs';
-import { useState } from 'react'
-
 
 const Desktop = (props) => {
-    const [openSnackBar, SetOpenSnackbar] = useState(false);
-    const [openSearch, SetOpenSearch] = useState(false);
-
-    const navigateDesk = useNavigate();
-
-    const elementInputRef = useRef();
-    const elementFormRef = useRef();
-    const elementDivRef = useRef();
-
-    const SearchOption = () => {
-        if (elementInputRef.current.value.length < 3) SetOpenSnackbar(true);
-        else navigateDesk('/busca');
-    }
-
-
-
-    const handleClose = (event, reason) => {
-        if (reason === 'clickaway') return;
-        SetOpenSnackbar(false);
-    };
-
-    const handleSubmit = event => {
-        event.preventDefault();
-        console.log('form submitted ✅');
-        SearchOption();
-    };
-
-
-    const handleClick = () => {
-        console.log(elementDivRef.current.style.display)
-
-        if (elementDivRef.current.style.display === 'flex') {
-
-            elementFormRef.current.submit();
-        }
-
-        if (elementDivRef.current.style.display === 'none')
-            console.log('k')
-    }
-
-
 
     return (
         <div
@@ -78,19 +35,7 @@ const Desktop = (props) => {
                 </div>
 
                 <div id='IconOption' className='col d-flex justify-content-end align-items-center me-4 gap-4'>
-                    <Tooltip title="Buscar">
-                        <button type='button' id='button-search-desk'
-                            style={{ display: 'flex' }} onClick={() => handleClick()}
-                            className={`bg-transparent text-${props.colorIsDarkOrLight}`}>
-                            <HiSearch className='fs-5' />
-                        </button>
-                    </Tooltip>
-
-                    <Snackbar open={openSnackBar} autoHideDuration={6000} onClose={handleClose}>
-                        <Alert onClose={handleClose} variant="filled" severity="error" sx={{ width: '100%' }}>
-                            Por favor, digite pelo menos 3 caracteres para pesquisar!
-                        </Alert>
-                    </Snackbar>
+                    <Search colorIsDarkOrLight={props.colorIsDarkOrLight}/>
 
                     <Tooltip title="Perfil">
                         <Link to={'/login'} className={`bg-transparent text-${props.colorIsDarkOrLight}`}><IoPerson className='fs-5 ' /></Link>
@@ -108,21 +53,6 @@ const Desktop = (props) => {
                         isDrawerCartOpen={props.isDrawerCartOpen} setIsDrawerCartOpen={props.setIsDrawerCartOpen}
                     />
                 </div>
-            </div>
-
-            <div
-                className='d-none justify-content-center align-items-center m-2 p-0 w-100 overflow-hidden'
-                style={{ height: '15vh', border: '5px solid var(--color)', borderRadius: '0px 0px 10px 10px' }}
-                ref={elementDivRef}
-            >
-                <form
-                    onSubmit={handleSubmit}
-                    ref={elementFormRef}
-                    id='form'
-                    className='search-form w-100 h-100 d-flex justify-content-center align-items-center p-0'
-                >
-                    <input ref={elementInputRef} placeholder='O QUE VOCÊ PROCURA?' className='search-field w-100 h-100' type="text" />
-                </form>
             </div>
         </div>
     )
---
+++ frontend/src/components/Search/Search.jsx
@@ -0,0 +1,57 @@
+import React, { useRef, useEffect, useState } from 'react'
+
+import { useNavigate } from 'react-router-dom'
+
+import { Alert, Snackbar, Tooltip } from '@mui/material';
+import { HiSearch } from 'react-icons/hi';
+
+import './search.css'
+
+const Search = ({colorIsDarkOrLight}) => {
+
+    const navigateDesk = useNavigate();
+
+    const [searchOpen, SetSearchOpen] = useState(0);
+
+    const elementInputRef = useRef();
+    const elementDivRef = useRef();
+
+    useEffect(() => {
+        SetSearchOpen(false);
+    }, []);
+
+    useEffect(() => {
+        if (!searchOpen) {
+            if (elementInputRef.current.value === '') {
+                elementInputRef.current.style.width = '0';
+                elementDivRef.current.style.background = 'none';
+                elementDivRef.current.style.padding = '0vh'
+            } else {
+                if (elementInputRef.current.value.length < 1) 
+                    SetOpenSnackbar(true);
+                else navigateDesk('/busca');
+            }
+        }
+        else {
+            elementInputRef.current.style.width = '25vh';
+            elementDivRef.current.style.background = 'var(--color)';
+            elementDivRef.current.style.padding = '0vh 3vh 0vh 3vh'
+        }
+    }, [searchOpen]);
+
+    return (
+        <Tooltip title="Buscar">
+            <div className='search-box' ref={elementDivRef}>
+                <input ref={elementInputRef} className='search-txt' type="text" placeholder="O que você procura?"  />
+                <button type='button' id='button-search-desk' onClick={(e) => {
+                    searchOpen ? SetSearchOpen(false) : SetSearchOpen(true);
+                }}
+                    className={`bg-transparent text-${colorIsDarkOrLight}`}>
+                    <HiSearch className='fs-5' />
+                </button>
+            </div>
+        </Tooltip>
+    )
+}
+
+export default Search
