214c389a950666482420bd50e72bf64ce0cf2c5b
Iasmin Dias <iasmin.dias87@gmail.com>
2022-12-02 13:45:20 -0300

    :eight_spoked_asterisk: NotFound Page

--- frontend/src/page/NotFound.jsx
+++ frontend/src/page/NotFound.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { ColorContext } from '../contexts/ColorContext'
+
+import Logo from './../components/Logo';
 
 const NotFound = () => {
+
+  const { getColor, colorIsWhiteOrBlack } = useContext(ColorContext);
+
+  useEffect(() => getColor('', true));
+
   return (
-    <div>NotFound</div>
+    <div className={`flex-column w-100 h-100 d-flex justify-content-center align-items-center text-${colorIsWhiteOrBlack} gap-3 p-4`}
+      style={{ backgroundColor: '#020202' }}
+    >
+      <Logo size={80}/>
+      <img 
+        style={{width: '50vh', height: '50vh'}}
+        className='rounded-circle'
+        src="https://i.makeagif.com/media/12-06-2015/wm6Fo7.gif" 
+        alt="404" 
+      />
+      <h1>404</h1>
+      <h3>PÁGINA NÃO ENCONTRADA</h3>
+    </div>
   )
 }
 
