85c7fb4129cfe2756eaa5dbbaf3bcc5520f945c0
cddmiasmin <iasmin.dias87@gmail.com>
2023-06-06 16:08:20 -0300

    .

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,10 +1,11 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 connection.connect((error)=>{
