2bf72e2c0420b0f7bbf912f7e022904ec69cfcc0
cddmiasmin <iasmin.dias87@gmail.com>
2023-06-15 10:49:27 -0300

    Update frontend

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,8 +1,7 @@
 import axios from 'axios';
-import { REACT_APP_API_URL } from '../../env';
 
 const api = axios.create({
-    baseURL: REACT_APP_API_URL,
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default api;
