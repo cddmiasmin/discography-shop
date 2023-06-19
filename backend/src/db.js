444a8ff32e4b66a98ce2250b25a7b85054b75878
Iasmin Dias <94560372+cddmiasmin@users.noreply.github.com>
2023-06-19 12:46:53 -0300

    Update db.js

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,17 +1,19 @@
-const mysql = require('mysql2');
+module.exports = fuction () {
+    const mysql = require('mysql2');
+    
+    const connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        port: process.env.DB_PORT
+    });
 
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
-});
+    connection.connect((error)=>{
+        if(error) throw error;
+        console.log(`Connected to database: ${process.env.DB_NAME}`);
+        connection.destroy();
+    });
 
-connection.connect((error)=>{
-    if(error) throw error;
-    console.log(`Connected to database: ${process.env.DB_NAME}`);
-    connection.destroy();
-});
-
-module.exports = connection;
+    return connection;
+}
