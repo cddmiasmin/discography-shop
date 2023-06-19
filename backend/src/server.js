af4b2bc99136ebad3b7823381e48a19a26ed3961
Iasmin Dias <94560372+cddmiasmin@users.noreply.github.com>
2023-06-19 12:42:18 -0300

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,7 @@ const routes = require('./routes.js');
 
 const server = express();
 server.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
     res.header("Access-Control-Allow-Headers",'X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization');
     server.use(cors());
