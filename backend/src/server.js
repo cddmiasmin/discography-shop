f7b4a3dea0d8b9ec12366a0b23db15677a6cc27e
cddmiasmin <iasmin.dias87@gmail.com>
2023-07-07 17:56:29 -0300

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,7 @@ const routes = require('./routes.js');
 const server = express();
  server.use(cors());
 server.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", 'https://discography-shop.vercel.app, https://discography-shop-cddmiasmin.vercel.app, https://discography-shop-git-main-cddmiasmin.vercel.app');
+    res.header("Access-Control-Allow-Origin", 'https://discography-shop.vercel.app, https://discography-shop-cddmiasmin.vercel.app, https://discography-shop-git-main-cddmiasmin.vercel.app, https://discography-shop-qyykwrpoz-cddmiasmin.vercel.app');
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
     res.header("Access-Control-Allow-Headers",'X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization');
     next();
